64bf4b508307d74087bd4b3a83cb3122f0c93a7e
ze-bang <46905987+ze-bang@users.noreply.github.com>
2021-12-01 09:57:26 -0500

    update datacq for automated addressline

--- meta_data_handler.py
+++ meta_data_handler.py
@@ -1,5 +1,7 @@
-import dataAcquisitionHelperFunctions as run
+import datetime
 
+import dataAcquisitionHelperFunctions as run
+import datetime
 
 class meta_data_handler():
     def __init__(self, frame):
@@ -10,13 +12,18 @@ class meta_data_handler():
     def grab_meta_data(self):
         counter = 0
         for widget in self.frame.winfo_children():
+            if counter == 5:
+                break
             if widget.winfo_class() == 'Entry':
                 self.metadata[counter] = widget.get()
                 counter = counter + 1
 
+        print(self.metadata)
     def runscan(self):
         self.grab_meta_data()
         counts = int(self.metadata[0])
+        time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        filename = self.metadata[1] + '\\' + self.metadata[0] + '_' + '_'.join(self.metadata[2:5]) + '_' + time + '.csv'
         trigParams, chanParams, timeParams, chanNumbers = run.setupOscilloscopeInput()
         dataAcq = run.dataAcquisition()
         dataAcq.prepareOscilloscope(triggerParameters=trigParams, channelParameters=chanParams,
@@ -24,4 +31,4 @@ class meta_data_handler():
         for i in tqdm(range(numOfIterations)):
             dataAcq.collectData(channels=chanNumbers)
         # Plot data
-        dataAcq.plotData(plotParameters=None)
+        dataAcq.plotData(plotParameters=None, filename = filename)
