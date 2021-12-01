f9bff0c32620d984438f5033a634fe8aa0e9747c
ze-bang <46905987+ze-bang@users.noreply.github.com>
2021-11-30 21:39:05 -0500

    inputfield creates both a label and an entry

---
+++ inputfield.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+
+class inputfield(tk.Frame):
+    def __init__(self, name=None,placeholder = 0, xpos=0, ypos=0, master=None, sep = 150, width=10):
+        super().__init__(master)
+        self.pack()
+        self.placeholder = str(placeholder)
+        self.xpos = xpos
+        self.ypos = ypos
+        self.sep = sep
+        self.width = width
+        if name == None:
+            print("please enter valid label name")
+        else:
+            self.create_widgets(name)
+
+
+    def create_widgets(self, name):
+        self.label = tk.Label(self.master,text=name)
+        self.entry = tk.Entry(self.master,width=self.width)
+        self.entry.insert(0, self.placeholder)
+        self.label.place(x=self.xpos,y=self.ypos)
+        self.entry.place(x=self.xpos+self.sep,y=self.ypos)
+
+    def get_entry(self):
+        return self.entry.get()
