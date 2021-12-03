24ae93f0e3b7ec40a496c4772627b6b451e8c857
ze-bang <46905987+ze-bang@users.noreply.github.com>
2021-12-03 11:37:28 -0500

    dummy test data

--- csv_reader.py
+++ csv_reader.py
@@ -5,7 +5,7 @@ from os.path import isfile, join
 
 
 def find_files(mypath):
-    onlyfiles = [[f.split('_')] for f in listdir(mypath) if isfile(join(mypath, f)) and f[0:8] == 'MATHUSLA']
+    onlyfiles = [f.split('_') for f in listdir(mypath) if isfile(join(mypath, f)) and f[0:8] == 'MATHUSLA']
     print(onlyfiles)
 
 find_files("./data/")
